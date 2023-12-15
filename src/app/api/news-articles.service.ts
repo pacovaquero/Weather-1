721d2ef686daad2a6f53ecbb099743ef5457f63b
pacovaquero <136713569+pacovaquero@users.noreply.github.com>
2023-12-15 10:42:14 -0300

    Add files via upload

---
+++ src/app/api/news-articles.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { NewsArticle } from '../news/news.page';
+import { NewsResponse } from '../news/news.page';
+
+const API_URL = environment.API_URL2;
+const API_KEY = environment.API_KEY2;
+
+@Injectable({
+ providedIn: 'root'
+})
+
+export class NewsArticlesService {
+
+ constructor(private httpClient:HttpClient) { 
+
+ }
+ getTopHeadLines(): Observable<NewsResponse> {
+    return this.httpClient.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${environment.API_KEY2}`);
+  
+ }
+
+ getArticleByCategory(category: string): Observable<NewsResponse> {
+    return this.httpClient.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${environment.API_KEY2}`);
+ }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_KEY1:'0f0f874987642ca7a595cd8ac7c08dcc',
+  API_URL1:'https://api.openweathermap.org/data/2.5/',
+  API_KEY2:'47738cb7612a499285189cf12e07d492',
+  API_URL2:'https://newsapi.org/v2/'
+};
